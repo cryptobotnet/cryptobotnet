ec034eb623cc5fcb34834bc86e200bb85a1d3546
Mikhail T <totalism@icloud.com>
2023-02-04 17:19:08 +0700

    fix(bot): rename firstMessage to introMessage

--- repos/apps/bot/src/commands/start/handler.ts
+++ repos/apps/bot/src/commands/start/handler.ts
@@ -5,18 +5,18 @@ import { redis } from 'lib/redis'
 export const handler = async (ctx: CommandContext<Context>) => {
   const { id: chatId } = ctx.chat
 
-  const firstResponseId = await redis.getFirstResponseId(chatId)
+  const introMessageId = await redis.getIntroMessageId(chatId)
 
-  const sendFirstMessage = async () => {
+  const sendIntroMessage = async () => {
     const { message_id } = await ctx.reply(Responses.START, {
       parse_mode: 'MarkdownV2'
     })
 
-    await redis.setFirstResponseId(chatId, message_id)
+    await redis.setIntroMessageId(chatId, message_id)
   }
 
-  if (!firstResponseId) {
-    sendFirstMessage()
+  if (!introMessageId) {
+    sendIntroMessage()
 
     return
   }
@@ -24,9 +24,11 @@ export const handler = async (ctx: CommandContext<Context>) => {
   try {
     await ctx.reply(Responses.HELP, {
       parse_mode: 'MarkdownV2',
-      reply_to_message_id: parseInt(firstResponseId)
+      reply_to_message_id: parseInt(introMessageId)
     })
+    /* NOTE: catch error if intro message was deleted */
+    /* NOTE: resend intro message and rewrite introMessageId */
   } catch (error) {
-    await sendFirstMessage()
+    await sendIntroMessage()
   }
 }
