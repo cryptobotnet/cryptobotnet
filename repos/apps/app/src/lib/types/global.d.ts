8e9a00e61627d329c33a61f87608f2aae19f557e
Mikhail T <totalism@icloud.com>
2023-02-08 03:43:07 +0700

    feat(app): switch antd theme with telegram colorScheme

--- repos/apps/app/src/lib/types/global.d.ts
+++ repos/apps/app/src/lib/types/global.d.ts
@@ -1,14 +1,15 @@
-declare module '*.svg' {
+export declare module '*.svg' {
   const content: unknown
   export default content
 }
 
-declare global {
+export declare global {
   interface Window {
     Telegram: {
       WebApp: {
         initData: string
         initDataUnsafe: any
+        colorScheme: 'light' | 'dark'
       }
     }
   }
--- repos/apps/app/src/pages/_app.page.tsx
+++ repos/apps/app/src/pages/_app.page.tsx
@@ -4,13 +4,16 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Script from 'next/script'
 
-import { ConfigProvider } from 'antd'
+import { ConfigProvider, theme } from 'antd'
 import { Layout } from 'components/layout'
 
 import 'lib/styles/global.css'
 import 'lib/styles/variables.css'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const telegramColorScheme =
+    typeof window !== 'undefined' && window?.Telegram?.WebApp?.colorScheme
+
   return (
     <>
       <Head>
@@ -22,6 +25,10 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
       <ConfigProvider
         componentSize="large"
         theme={{
+          algorithm:
+            telegramColorScheme === 'dark'
+              ? theme.darkAlgorithm
+              : theme.defaultAlgorithm,
           token: {
             borderRadius: 40
           }
