6bf06088efdb89957dd8d9b2750804812bff1e5c
Mikhail T <totalism@icloud.com>
2023-02-24 18:36:27 +0700

    chore(server): add test endpoint

--- repos/apps/server/src/components/router.ts
+++ repos/apps/server/src/components/router.ts
@@ -8,9 +8,17 @@ export const router = Router()
 router.post(Endpoints.SUBSCRIBE_INSTRUMENT, (req, res) => {
   const { instrumentId } = req.body
 
+  if (!instrumentId) {
+    return
+  }
+
   debug(`received request to subscribe to instrument`, instrumentId)
 
   wsManager.subscribeInstrument(instrumentId)
 
   return res.status(200).end()
+})
+
+router.get('/test', (req, res) => {
+  return res.status(200).send('got GET request on /test endpoint').end()
 })
